c460e1a97b462d0ec1f75bb5aa643cb113a43be9
Sade Paula <sadepaulaaquino@gmail.com>
2019-04-18 19:48:22 -0500

    C:/Program Files/Git/welcome works, no validation

--- user-signup/main.py
+++ user-signup/main.py
@@ -1,45 +1,55 @@
 from flask import Flask, request
+import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-form= """
+
+page_header = """
 <!DOCTYPE html>
-<html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-5" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Validation Example</title>
+    <link rel="stylesheet" href="/static/app.css" />
+  </head>
+  <body>
+  """
+
+page_footer = """
 <body>
-    <h1> My Sign-Up Form </h1>
-    <form action="/welcome" method="POST">
+<html>"""
+
+signup_form= """
+<form action="/welcome" id="form" method="POST">
+        <h1> Signup Form </h1>
         <label for="username">Username:</label>
-        <input id="username" type="text" name="username"/>
-
-        <br>
-        <br>
-
+        <input id="username" type="text" name="username" value= "{0}"/>
+        <p class="error"> {1} </p>    
         <label for="password1">Write Password:</label>
-        <input id= password1 type="password" name= "password1"/>
-
-         <br>
-         <br>
-
+        <input id= password1 type="password" name= "password1" value= "{2}"/>
+         <p class="error"> {3} </p> 
         <label> Rewirte Password:
-        <input id="password2" type="password" name= "password2"/>
+        <input id="password2" type="password" name= "password2" value= "{4}"/>
+        <p class="error"> {5} </p> 
         </label>
-        
-        <input type="submit"/>
-
-
+        <button type="submit">Submit</button>
     </form>
-</body>
-</html>
 """
-@app.route ("/welcome", methods=['POST'])
-def welcome():
-    username= request.form ["username"]
-    return  '<h1>Welcome,' + username + '</h1>'
-        
-        
+
 
 @app.route("/")
 def index():
-    return form
+    content= page_header + signup_form.format("", "", "", "", "","") + page_footer
+    return content 
 
+@app.route("/welcome", methods=['POST'])
+def welcome():
+    username = cgi.escape(request.form ['username'])
+    
+    welcome_message = '<h1>Welcome, ' + username + '</h1>'
+    content = page_header + welcome_message + page_footer
+    return content
+        
 app.run()
